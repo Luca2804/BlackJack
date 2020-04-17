53fe411be5f034739fcf1b31625b0de41d23aac6
Luca2804 <62542481+Luca2804@users.noreply.github.com>
2020-04-17 12:39:43 +0200

    Add files via upload

--- Spielablauf.java
+++ Spielablauf.java
@@ -15,6 +15,7 @@ public class Spielablauf {
 	boolean verloren;
 	boolean verlorenbank;
 	int Zaehler = 0;
+	String weiterspielen = "";
 
 	public void BlackJack() {
 
@@ -23,19 +24,26 @@ public class Spielablauf {
 		SpielerAnzahl();
 		setSpieler();
 		bankerstellen();
-		Einsaetzen();
-		KartenausteilenundKartenzeigen();
-		bankzieht();
-		nochmalziehenSpieler();
-		Banknochmalziehen();
-		ErmittelenGewinner();
-
+		do {
+			Einsetzen();
+			KartenausteilenundKartenzeigen();
+			bankzieht();
+			Verdoppeln();
+			nochmalziehenSpieler();
+			Banknochmalziehen();
+			ErmittelnGewinner();
+			resetPunkte();
+			Spielerraus();
+			System.out.print("Wollen sie weiter spielen (J oder N): ");
+			System.out.println();
+			weiterspielen = scan.nextLine();
+		} while (!weiterspielen.equals("N"));
 	}
 
 	private void GeldEingeben() {
 
 		System.out.println();
-		System.out.println("Geben sie das Geld ein für jeden Spieler das gleiche Startkapital");
+		System.out.print("Geben sie das Geld ein für jeden Spieler das gleiche Startkapital: ");
 		Geld = scan.nextInt();
 		System.out.println("Jeder Spieler hat " + Geld + " Chips");
 
@@ -46,7 +54,7 @@ public class Spielablauf {
 		System.out.println();
 
 		while (Spieleranzahl < 7) {
-			System.out.println("Geben sie die Anzahl der Spieler ein (1-7)");
+			System.out.print("Geben sie die Anzahl der Spieler ein (1-7): ");
 			Spieleranzahl = scan.nextInt();
 			if (Spieleranzahl > 7) {
 				System.out.println("Ungueltige Spieleranzahl");
@@ -66,7 +74,7 @@ public class Spielablauf {
 			spieler[i] = new Spieler();
 
 			System.out.println();
-			System.out.println("Spieler " + (i + 1) + " wie ist dein Name");
+			System.out.print("Spieler " + (i + 1) + " wie ist dein Name: ");
 			if (i == 0) {
 				scan.nextLine();
 			}
@@ -75,6 +83,7 @@ public class Spielablauf {
 
 			spieler[i].setGeld(Geld);
 		}
+		System.out.println();
 	}
 
 	private void bankerstellen() {
@@ -83,18 +92,22 @@ public class Spielablauf {
 
 	}
 
-	private void Einsaetzen() {
+	private void Einsetzen() {
 
-		for (int i = 0; i < spieler.length; i++) {
+		int Geld = 0;
+
+		for (int i = 0; i < Spieleranzahl; i++) {
 			do {
-				System.out.println(spieler[i].name + " Geben sie ihren Einsatz ein:");
+				System.out.print(spieler[i].name + " Geben sie ihren Einsatz ein: ");
 				spieler[i].Einsatz = scan.nextInt();
+				System.out.println();
+				Geld = spieler[i].geld;
 				if (spieler[i].Einsatz > spieler[i].geld) {
 					System.out.println("Sie haben nicht so viele Chips");
 				} else {
 					spieler[i].geld = spieler[i].geld - spieler[i].Einsatz;
 				}
-			} while (spieler[i].Einsatz > spieler[i].geld);
+			} while (spieler[i].Einsatz > Geld);
 		}
 	}
 
@@ -116,45 +129,50 @@ public class Spielablauf {
 			bank.HandKarteHinzufuegen(kartendeck.giveNextCard());
 			bank.HandkartenWert();
 		}
-		System.out.println();
 		System.out.println("Die Bank hat: " + bank.getPunkte() + " Punkte auf der Hand");
+		System.out.println();
 	}
 
 	public void nochmalziehenSpieler() {
 
 		scan.nextLine();
 		for (Zaehler = 0; Zaehler < Spieleranzahl; Zaehler++) {
-			verloren = false;
-			do {
-				System.out.println(spieler[Zaehler].getName() + " Wollen sie nochmal ziehen (J oder N)");
-				ziehen = scan.nextLine();
-				if (ziehen.equals("J") || ziehen.equals("j")) {
-					spieler[Zaehler].HandKarteHinzufuegen(kartendeck.giveNextCard());
-					spieler[Zaehler].HandkartenWert();
-					System.out.println(spieler[Zaehler].getName() + " Handkarte: " + spieler[Zaehler].getKarte()
-							+ " Handkartenwert: " + spieler[Zaehler].getPunkte());
-					Ueberkauft();
-					if (spieler[Zaehler].verloren) {
-						break;
+			spieler[Zaehler].verloren = false;
+			if (spieler[Zaehler].verdoppeln == false) {
+				do {
+					System.out.print(spieler[Zaehler].getName() + " Wollen sie nochmal ziehen (J oder N): ");
+					System.out.println();
+					ziehen = scan.nextLine();
+					if (ziehen.equals("J") || ziehen.equals("j")) {
+						spieler[Zaehler].HandKarteHinzufuegen(kartendeck.giveNextCard());
+						spieler[Zaehler].HandkartenWert();
+						System.out.println(spieler[Zaehler].getName() + " Handkarte: " + spieler[Zaehler].getKarte()
+								+ " Handkartenwert: " + spieler[Zaehler].getPunkte());
+						Ueberkauft();
+						if (spieler[Zaehler].verloren) {
+							break;
+						}
 					}
-				}
-			} while (!ziehen.equals("N"));
+				} while (!ziehen.equals("N"));
+			}
 		}
 	}
 
 	private void Ueberkauft() {
 		if (21 < spieler[Zaehler].getPunkte()) {
 			System.out.println(spieler[Zaehler].getName() + " Sie haben verloren!");
+			System.out.println();
 			spieler[Zaehler].verloren(verloren);
 		}
 	}
 
 	private void Banknochmalziehen() {
 
-		while (16 > bank.getPunkte()) {
+		while (17 > bank.getPunkte()) {
 			bank.HandKarteHinzufuegen(kartendeck.giveNextCard());
 			bank.HandkartenWert();
 			System.out.println("Die Bank hat: " + bank.getPunkte());
+			System.out.println();
 			BankUeberkauft();
 			if (verlorenbank) {
 				break;
@@ -166,6 +184,7 @@ public class Spielablauf {
 
 		if (21 < bank.getPunkte()) {
 			System.out.println("Die Bank hat sich überkauft");
+			System.out.println();
 			verlorenbank = true;
 		}
 	}
@@ -176,14 +195,93 @@ public class Spielablauf {
 			if (spieler[i].verloren) {
 				System.out.println(spieler[i].getName() + " sie haben verloren!");
 				System.out.println(spieler[i].getName() + " sie haben noch " + spieler[i].geld + " Chips");
+				System.out.println();
 			} else if (verlorenbank == true && spieler[i].verloren == false) {
 				System.out.println(spieler[i].getName() + " sie haben gewonnen!");
 				spieler[i].geld = spieler[i].geld + spieler[i].Einsatz * 2;
 				System.out.println(spieler[i].getName() + " sie haben jetzt " + spieler[i].geld + " Chips");
+				System.out.println();
 			} else if (spieler[i].getPunkte() > bank.getPunkte() && verlorenbank == false) {
 				System.out.println(spieler[i].getName() + " sie haben gewonnen!");
 				spieler[i].geld = spieler[i].geld + spieler[i].Einsatz * 2;
 				System.out.println(spieler[i].getName() + " sie haben jetzt " + spieler[i].geld + " Chips");
+				System.out.println();
+			} else if (bank.getPunkte() > spieler[i].getPunkte()) {
+				System.out.println(spieler[i].getName() + " sie haben verloren!");
+				System.out.println(spieler[i].getName() + " sie haben noch " + spieler[i].geld + " Chips");
+				System.out.println();
+			} else if (bank.getPunkte() == spieler[i].getPunkte()) {
+				System.out.println(spieler[i].getName() + " Unentschieden");
+				System.out.println();
+				spieler[i].geld = spieler[i].geld + spieler[i].Einsatz;
+				System.out.println(spieler[i].getName() + " sie haben noch " + spieler[i].geld + " Chips");
+				System.out.println();
+			}
+		}
+		verlorenbank = false;
+	}
+
+	private void resetPunkte() {
+		for (int i = 0; i < Spieleranzahl; i++) {
+			spieler[i].resetKartenwert();
+			spieler[i].resetKartenaufhand();
+		}
+		bank.resetKartenwert();
+	}
+
+	private void Spielerraus() {
+
+		int anzahlspielerraus = 0;
+
+		for (int i = 0; i < Spieleranzahl; i++) {
+			if (spieler[i].geld == 0) {
+				System.out.println(spieler[i].getName() + " sie sind raus");
+				System.out.println();
+				anzahlspielerraus++;
+
+				for (int j = i; j < Spieleranzahl; j++) {
+					if (j < (Spieleranzahl - 1)) {
+						spieler[i] = spieler[i + 1];
+					}
+				}
+				if (i < Spieleranzahl - 1) {
+					i--;
+				}
+			}
+			if (anzahlspielerraus == Spieleranzahl) {
+				System.out.println("Alle Spieler haben keine Chips mehr Porgramm wird beendet");
+				System.exit(0);
+			}
+		}
+		Spieleranzahl -= anzahlspielerraus;
+	}
+
+	private void Verdoppeln() {
+
+		String verdoppeln = " ";
+
+		scan.nextLine();
+		for (int i = 0; i < Spieleranzahl; i++) {
+			System.out.print(spieler[i].getName() + " wollen sie verdoppeln (J oder N): ");
+			verdoppeln = scan.nextLine();
+			System.out.println();
+			if (verdoppeln.equals("J")) {
+				spieler[i].geld = spieler[i].geld - spieler[i].Einsatz;
+				spieler[i].Einsatz = spieler[i].Einsatz * 2;
+				spieler[i].verdoppeln = true;
+				System.out.println(spieler[i].getName() + " sie haben ihren Einsatz veroppelt!");
+				spieler[i].HandKarteHinzufuegen(kartendeck.giveNextCard());
+				spieler[i].HandkartenWert();
+				System.out.println(spieler[i].getName() + " Handkarte: " + spieler[i].getKarte() + " Handkartenwert: "
+						+ spieler[i].getPunkte());
+				if (21 < spieler[i].getPunkte()) {
+					System.out.println(spieler[i].getName() + " Sie haben verloren!");
+					System.out.println();
+					verloren = true;
+					spieler[i].verloren(verloren);
+				}
+			} else {
+				spieler[i].verdoppeln = false;
 			}
 		}
 	}
